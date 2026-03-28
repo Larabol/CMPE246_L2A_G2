65aa86deab7708552dd0a5f254d875b2ff26a8fb
DJSaiKo <simondevanhoughton@gmail.com>
2026-03-27 22:30:00 -0700

    Added cell holder CAD files and some photos. Updated bmsmain to work with Data_Preprocessing.

--- src/bmsmain.py
+++ src/bmsmain.py
@@ -1,8 +1,10 @@
-import smbus2
 import src.smbusutils as smbu
 import pandas as pd
 from collections import deque
 import time
+from datetime import datetime
+import Data_Preprocessing
+import ML_Model
 
 addr = 0xb
 cell_capacity = 3350
@@ -13,19 +15,24 @@ buffer = deque(maxlen=max_points)
 
 bms = smbu.BMS(addr, cell_capacity, bus_num)
 
+preprocessor = Data_Preprocessing.DataPreprocessingScript(
+    "bmsdata.csv",
+    "processedbmsdata.csv"
+)
+
 def get_data():
     return {
-        "time": time.time(),
+        "timestamp": datetime.now().isoformat(),
         "voltage": bms.get_pack_voltage(),
         "temperature": bms.get_temperature(),
         "current": bms.get_current(),
-        "coulomb_count": bms.get_coulombs()
     }
 
-#jacob was here
 while True:
     buffer.append(get_data())
-    df = pd.dataFrame(buffer)
+    df = pd.DataFrame(buffer)
     df.to_csv("bmsdata.csv", index=False)
+    if len(buffer) % 50 == 0:
+        preprocessor.preprocess_data()
 
     time.sleep(1)
