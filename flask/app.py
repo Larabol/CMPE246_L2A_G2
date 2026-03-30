f76e3db685e7fed2d587496fec2d993a7ff0299c
Larabol <Laraboljuncic@gmail.com>
2026-03-30 00:01:42 -0700

    more webpage

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 import pandas as pd
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import DatetimeTickFormatter
 
 app = Flask(__name__)
 
@@ -9,11 +12,51 @@ def index():
     # Read CSV file
     df = pd.read_csv("bmsdata.csv")
 
-    # Convert dataframe to a dictionary for easy access in HTML
-    data = df.to_dict(orient="records")
+    # Rename columns for easier access in Jinja
+    df.rename(columns={
+    'timestamp': 'time',
+    'voltage': 'voltage',
+    'current': 'current',
+    'temperature': 'temperature',
+    'soc': 'soc'
+    }, inplace=True)
 
-    # Send data to HTML template
-    return render_template("index.html", data=data)
+    # Convert dataframe to list of dictionaries
+    data = df.to_dict(orient='records')
+
+    df['time'] = pd.to_datetime(df['time'])
+
+    # Create Bokeh figure
+    p = figure(
+        title="Voltage vs Time", 
+        y_axis_label="Voltage (mV)",
+        x_axis_label="Time(???)",
+        x_axis_type= 'datetime',
+        height=300, 
+        sizing_mode="stretch_width"
+    )
+
+    x_values = df['time'][:50]
+    y_values = df['voltage'][:50]
+
+    p.xaxis.formatter = DatetimeTickFormatter(
+        seconds="%H:%M:%S",
+        minutes="%H:%M",
+        hours="%H:%M"
+    )
+    
+    p.line(x_values, y_values, line_width=2, color="navy", alpha=0.7)
+
+    # Extract script and div components
+    script, div = components(p)
+
+    # Debugging: Print script and div to check if they are generated
+    print("Generated Script:", script[:50])
+    print("Generated Div:", div[:50])
+    print(df.columns)
+
+    # Render an HTML template with the components
+    return render_template('index.html', data=data, script=script, div=div)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
