0ecba3e07861a86e5aa68bd8c0eb57ddb601a172
sknight11716 <sam1101@student.ubc.ca>
2026-03-30 00:20:19 -0700

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import DatetimeTickFormatter
+import json
+import os
 
 app = Flask(__name__)
 
@@ -57,6 +59,17 @@ def index():
 
     # Render an HTML template with the components
     return render_template('index.html', data=data, script=script, div=div)
+
+## Route for json data
+@app.route("/latest_status")
+def latest_status():
+    if not os.path.exists("tatest_status.json"):
+        return jsonify({"Error": "latest_status.json not found"}), 404
+        
+    with open("latest_status.json", "r") as f:
+        status_data = json.load(f)
+        
+    return jsonify(status_data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
