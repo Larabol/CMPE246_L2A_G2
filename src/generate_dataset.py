8972b2a84f7d47520ca590adcf1325eac0103adc
DJSaiKo <simondevanhoughton@gmail.com>
2026-03-30 12:44:11 -0700

    Added generate_dataset

---
+++ src/generate_dataset.py
@@ -0,0 +1,31 @@
+import time
+from smbusutils import BMS
+from Data_Preprocessing import DataPreprocessingScript
+
+addr = 0x0b
+raw_data_file = "bmsdata.csv"
+processed_data_file = "battery_data_processed.csv"
+
+bms = BMS(addr)
+
+preprocessor = DataPreprocessingScript(
+    input_file=raw_data_file,
+    output_file=processed_data_file,
+    battery_capacity_ah = 16.75,  # Specific to battery
+    full_charge_voltage = 20.5,  # Specific to battery
+    full_charge_current_threshold = 0.2,  ## Specific to battery
+)
+
+def update_data():
+    # generate new raw CSV
+    bms.write_raw_data(raw_data_file)
+
+    # preprocess latest raw CSV
+    preprocessor.preprocess_data() 
+
+if __name__ == "__main__":
+    for i in range(1000):
+        
+        update_data()
+        print("updated data") 
+        time.sleep(2)
